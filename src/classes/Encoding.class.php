ff25abea7a3cf2a612abf13181ed46951a2611d4
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-03-16 17:25:53 +0100

    added decypher

--- src/classes/Encoding.class.php
+++ src/classes/Encoding.class.php
@@ -4,6 +4,10 @@
       return ord(utf8_encode($lettre));
     }
 
+    private function chiffre_lettre($chiffre) {      
+      return chr($chiffre);
+    }
+
     public static function chaine_utf8($mot) {
       $arr = str_split($mot);
       $result = array();
@@ -11,6 +15,14 @@
         $result[] = Encoding::lettre_chiffre($lettre);
       }
       return $result;
+    }
+
+    public static function chiffre_utf8($code_chiffre) {
+      $text = '';
+      foreach ($code_chiffre as $chiffre) {
+        $text = $text . Encoding::chiffre_lettre($chiffre);
+      }
+      return utf8_encode($text);
     }
   }
 ?>
--- src/encoding.php
+++ src/encoding.php
@@ -2,8 +2,13 @@
   include_once("classes/Encoding.class.php");
 
   function encoding1() {
-    echo json_encode(Encoding::chaine_utf8("ami"));
+    echo json_encode(Encoding::chaine_utf8("ami")) . "\n";
+  }
+
+  function encoding2() {
+    echo Encoding::chiffre_utf8(Encoding::chaine_utf8("ami")) . "\n";
   }
 
   encoding1();
+  encoding2();
 ?>

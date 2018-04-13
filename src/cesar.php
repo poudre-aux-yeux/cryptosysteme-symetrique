605c6f63a178c34cd61c47dcc27b9d2c3af61276
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-04-13 15:08:22 +0200

    incorpore la table de permutation dans le cryptosystème

--- src/cesar.php
+++ src/cesar.php
@@ -3,22 +3,22 @@ include_once("classes/Cesar.class.php");
 
 function cesar1() {
   echo "cesar1 : césarification d'un mot (AMI)\n";
-  echo json_encode(Cesar::code_mot_chiffre('AMI')) . "\n_\n";
+  echo json_encode(Cesar::code_mot_chiffre('AMI')) . "\n\n";
 }
 
 function cesar2() {
   echo "cesar2 : dé-césarification d'un mot césarifié (AMI)\n";
-  echo Cesar::code_chiffre_mot(Cesar::code_mot_chiffre('AMI')) . "\n_\n";
+  echo Cesar::code_chiffre_mot(Cesar::code_mot_chiffre('AMI')) . "\n\n";
 }
 
 function cesar3() {
   echo "cesar3 : cryptage d'un mot (AMI)\n";
-  echo json_encode(Cesar::crypto('AMI')) . "\n_\n";
+  echo json_encode(Cesar::crypto('AMI')) . "\n\n";
 }
 
 function cesar4() {
   echo "cesar4 : décryptage d'un mot (AMI)\n";
-  echo Cesar::uncrypto(Cesar::crypto('AMI')) . "\n_\n";  
+  echo Cesar::uncrypto(Cesar::crypto('AMI')) . "\n\n";  
 }
 
 cesar1();
--- src/encoding.php
+++ src/encoding.php
@@ -11,6 +11,28 @@
     echo Encoding::binary_to_text(Encoding::text_to_binary("AMI")) . "\n\n";
   }
 
+  function encoding3() {
+    echo "encoding3 : every day I'm shuffling (01001101)\n";
+    $shuffled = Encoding::shuffle("01001101");
+    echo $shuffled. "\n";
+    echo "and un-shuffling\n";
+    echo Encoding::unshuffle($shuffled) . "\n\n";
+  }
+
+  function encoding4() {
+    echo "encoding4 : cryptage complet d'une chaîne (AMI)\n";
+    echo Encoding::crypto("AMI") . "\n\n";
+  }
+
+  function encoding5() {
+    echo "encoding5 : décryptage complet d'une chaîne (AMI)\n";
+    $encrypted = Encoding::crypto("AMI");
+    echo Encoding::uncrypto($encrypted);
+  }
+
   encoding1();
   encoding2();
+  encoding3();
+  encoding4();
+  encoding5();
 ?>
